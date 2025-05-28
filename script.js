f1a2f9cabab9f2fb0258a2d127ad36b003263bc5
Mouatamid <116890602+mutamid1@users.noreply.github.com>
2025-05-28 10:47:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,51 @@ fetch('testi.json')
     document.getElementById('hero-description').innerHTML = data.hero.description;
     document.getElementById('intro').innerHTML = data.introduzione.titolo;
     document.getElementById('intro-testo').innerHTML = data.introduzione.testo;
+    document.getElementById('p3-sub').innerHTML = data.domande.sottotitolo;
+    document.getElementById('p3-title').innerHTML = data.domande.titolo;
+    document.getElementById('q1').innerHTML = data.domande.domanda1;
+    document.getElementById('q2').innerHTML = data.domande.domanda2;
+    document.getElementById('q3').innerHTML = data.domande.domanda3;
+    document.getElementById('q4').innerHTML = data.domande.domanda4;
+    document.getElementById('q5').innerHTML = data.domande.domanda5;
   });
+
+
+
+  function changeLanguage(lang){
+    if (lang === 'it'){
+        fetch('testi.json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('hero-title').innerHTML = data.hero.title;
+            document.getElementById('hero-subtitle').innerHTML = data.hero.subtitle;
+            document.getElementById('hero-description').innerHTML = data.hero.description;
+            document.getElementById('intro').innerHTML = data.introduzione.titolo;
+            document.getElementById('intro-testo').innerHTML = data.introduzione.testo;
+            document.getElementById('p3-sub').innerHTML = data.domande.sottotitolo;
+            document.getElementById('p3-title').innerHTML = data.domande.titolo;
+            document.getElementById('q1').innerHTML = data.domande.domanda1;
+            document.getElementById('q2').innerHTML = data.domande.domanda2;
+            document.getElementById('q3').innerHTML = data.domande.domanda3;
+            document.getElementById('q4').innerHTML = data.domande.domanda4;
+            document.getElementById('q5').innerHTML = data.domande.domanda5;
+            });
+        } else if (lang === 'en'){
+        fetch('dates.json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('hero-title').innerHTML = data.hero.title;
+            document.getElementById('hero-subtitle').innerHTML = data.hero.subtitle;
+            document.getElementById('hero-description').innerHTML = data.hero.description;
+            document.getElementById('intro').innerHTML = data.introduzione.titolo;
+            document.getElementById('intro-testo').innerHTML = data.introduzione.testo;
+            document.getElementById('p3-sub').innerHTML = data.domande.sottotitolo;
+            document.getElementById('p3-title').innerHTML = data.domande.titolo;
+            document.getElementById('q1').innerHTML = data.domande.domanda1;
+            document.getElementById('q2').innerHTML = data.domande.domanda2;
+            document.getElementById('q3').innerHTML = data.domande.domanda3;
+            document.getElementById('q4').innerHTML = data.domande.domanda4;
+            document.getElementById('q5').innerHTML = data.domande.domanda5;
+            });
+    }
+}
